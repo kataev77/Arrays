c8853a4a87b2f2fe9bed62bbf4ce92bfdac28d59
Kataev <Kataev.hamzat@mail.ru>
2024-02-02 14:58:02 +0300

    repair1

--- function.js
+++ function.js
@@ -1,46 +1,67 @@
+/*const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function calcSum () {
+    const result = [ ]
+     for(let i = 0; i < arr.length; i++) {
+        return result += arr[i]
+     }
+}
+
+console.log(calcSum(arr))*/
+
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function func (sum) {
-     for(let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i]
-     }
-     return sum
-}
-console.log(func(0))
-
-
-
-
-let arr_1 = [-1, 2, -2, 2, 3, -3, 4, 5, 6, 7, 8, 9];
-
-function func_1(summa) {
-    for(let i = 0; i < arr_1.length; i++) {
-        if(arr_1[i] > 0){
-            summa += arr_1[i]
-        }
+function calcSum() {
+    let result = 0
+    for(let i = 0; i < arr.length; i++) {
+        result += arr[i]
     }
-    return summa
+    return result
 }
-console.log(func_1(0))
+console.log(calcSum(arr))
+
+
+
+const arr_1 = [-1, 2, -2, 2, 3, -3, 4, 5, 6, 7, 8, 9];
+
+function func_1() {
+    let result1 = 0
+    for(let i = 0; i < arr_1.length; i++) {
+        if(arr_1[i] >= 0){
+            result1 += arr_1[i]
+        }
+}
+return result1
+}
+console.log(func_1(arr_1))
+
+
 
 
 
 const arr_3 = ['html', 'css', 'python', 'github', 'react', 'javaScript', 'fs', 'qr', 'jv', 'qrwr'];
 
-function func_2(string) {
+function func_2() {
+    const result = [ ]
+    
     for(let i = 0; i < arr_3.length; i++) {
         if(arr_3[i].length > 3){
-            string.push(arr_3[i])
+            result.push(arr_3[i])
         }
     }
-    return string
+    return result
 }
-console.log(func_2([ ]));
+console.log(func_2(arr_3))
+
+
+
+
 
 
 const arr_4 = ['a', 'b', 'c', 'y', 'a']
 
-function func_3(result){
+function func_3(){
+    let result = [ ]
     for(let i = 0; i < arr_4.length; i++) {
         if(arr_4[i][0] === 'a'){
             result.push(arr_4[i])
@@ -48,7 +69,7 @@ function func_3(result){
     }
     return result
 }
-console.log(func_3([ ]))  
+console.log(func_3(arr_4));
 
 
 
